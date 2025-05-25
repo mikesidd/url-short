f3bcae6d84b3511cb3ed5eecc43d199663f3403d
SIddharth <kingkingdurjay@gmail.com>
2025-05-25 13:14:15 +0530

    chore: update Next.js config to disable ESLint and TypeScript checks during build

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
+    dirs: ['app']
+  },
+  typescript: {
+    ignoreBuildErrors: true
   },
   /* config options here */
 };
